c0cb50ac2ff37005ae03b0a1f9690e11df20144c
OMatematico-coder <servidor@omatematico.net.br>
2026-08-17 08:40:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,5 +6,9 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+
+  // Adicione esta linha para ignorar requisições POST e deixar o Google Script trabalhar em paz
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
